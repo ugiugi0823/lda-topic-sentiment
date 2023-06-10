22940e02d6edf275f808677b7f2c4df4da71baba
Hyunwook Jo <acerghjk@gmail.com>
2023-06-10 16:53:14 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ cd /content/lda_topic_sentiment
 gdown '1wdTqzP202eQLirHApsybI8bIiROBU_H-&confirm=t'
 unzip pickle_db_file.zip -d ./file/
 
-
+rm -r cardiffnlp
 python main.py --lda 'lda_6_9.pickle' \
   --db 'url_check_6_8.db' \
   --n_topic 10 \
