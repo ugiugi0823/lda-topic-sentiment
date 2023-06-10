9de05ebd3d92e5db56b07823ee13e3ab8bea7c32
Hyunwook Jo <acerghjk@gmail.com>
2023-06-10 17:42:13 +0900

    Create get_score.py

---
+++ get_score.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from correlation import get_correlation
+def get_score()
+  for i in range(10):
+      df = pd.read_csv(
+          '/content/lda_topic_sentiment/dd/' + 'topic_' + str(
+              i) + '_text.csv', lineterminator='\n')
+      print(f'{i}번쨰_______________________________________')
+      print(get_correlation(df, snp))
+      # print(type(a))
+      # print(type(b))
