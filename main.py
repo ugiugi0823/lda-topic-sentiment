9ec0dbcddf64fc1dcbb8378609351cbddd9b156d
Hyunwook Jo <acerghjk@gmail.com>
2023-06-10 21:53:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ if __name__ == '__main__':
   p.add_argument("--sentiment", default="cardiffnlp/twitter-roberta-base-sentiment-latest", help="lda_model 경로를 넣어주세요!")
   p.add_argument("--n_topic", type=int , default=10, help="몇개의 토픽인지 적어주세요!")
   p.add_argument("--topic_dir", default="./lda_topic_sentiment/result", help="토픽별 감성분석 결과를 담는 경로")
-  p.add_argument("--output_dir", default="./lda_topic_sentiment/output", help="혹시 몰라서 전체 토빅별 감성 결과를 담는 경로")
   p.add_argument("--topic_text_dir", default="./lda_topic_sentiment/topic_text", help="lda 후 나온 토픽별 csv 경로")
 
   args = p.parse_args()
