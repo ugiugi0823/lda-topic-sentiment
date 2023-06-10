c8225b59899a38c83a191fd59ecd8d25d973103c
Hyunwook Jo <acerghjk@gmail.com>
2023-06-10 18:16:28 +0900

    Update get_score.py

--- get_score.py
+++ get_score.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from etc.correlation import get_correlation
+from correlation import get_correlation
 
 snp = [4145.58, 4115.24, 4151.28, 4205.45, 4205.52, 4205.52, 4205.52, 4205.52, 4179.83, 4221.02, 4282.37, 4279.37,
        4276.37, \
@@ -9,7 +9,7 @@ snp = [4145.58, 4115.24, 4151.28, 4205.45, 4205.52, 4205.52, 4205.52, 4205.52, 4
 def get_score():
   for i in range(10):
       df = pd.read_csv(
-          '/content/lda_topic_sentiment/dd/' + 'topic_' + str(
+          '/content/drive/MyDrive/sw/project/LDA/result/' + 'topic_' + str(
               i) + '_text.csv', lineterminator='\n')
       print(f'{i}번쨰_______________________________________')
       print(get_correlation(df, snp))
