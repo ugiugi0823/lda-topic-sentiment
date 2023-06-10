7acf0eccbfe2730bef0a766adc0dd0206489b8a4
Hyunwook Jo <acerghjk@gmail.com>
2023-06-10 17:44:57 +0900

    Update get_score.py

--- get_score.py
+++ get_score.py
@@ -1,5 +1,11 @@
 import pandas as pd
 from correlation import get_correlation
+
+snp = [4145.58, 4115.24, 4151.28, 4205.45, 4205.52, 4205.52, 4205.52, 4205.52, 4179.83, 4221.02, 4282.37, 4279.37,
+       4276.37, \
+       4273.79, 4283.85, 4267.52, 4293.93]
+
+
 def get_score():
   for i in range(10):
       df = pd.read_csv(
