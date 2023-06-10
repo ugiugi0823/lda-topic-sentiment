75e3cd95ea59c7f996b515bd942d68ed1b005a48
Hyunwook Jo <acerghjk@gmail.com>
2023-06-10 16:42:32 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,11 @@
 pip install transformers
-cd ./lda_topic_sentiment
-gdown '1-nX8JfYDyZEo7fmI5SRhnBbhaPQ-dRl6&confirm=t'
-unzip lda_6_8.zip
-# lda_6_7_total.pickle
-gdown '1_zn7_5TB6ZngKqWjp6uaKWGMgydHSHhw&confirm=t'
-# lda_6_9.pickle
-gdown '1IdegrqkFzPUXLskVAOc5VPN65sGyQyen&confirm=t'
+git clone https://github.com/ugiugi0823/lda_topic_sentiment.git
+cd /content/lda_topic_sentiment
+gdown '1wdTqzP202eQLirHApsybI8bIiROBU_H-&confirm=t'
+unzip pickle_db_file.zip -d ./file/
 
-python main.py --ldaa 'lda_6_9.pickle' \
+
+python main.py --lda 'lda_6_9.pickle' \
   --db 'url_check_6_8.db' \
   --n_topic 10 \
   --sentiment "cardiffnlp/twitter-roberta-base-sentiment-latest" \
