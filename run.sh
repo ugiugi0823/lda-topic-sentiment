18636a9cb7af55f7ce633fdb75f1efe28d5b6b3d
Hyunwook Jo <acerghjk@gmail.com>
2023-06-17 17:42:09 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 # 최종 날짜 2023.06.18, 반영한 DB
 pip install transformers
 cd /content/lda_topic_sentiment
-gdown '1wdTqzP202eQLirHApsybI8bIiROBU_H-&confirm=t'
-unzip pickle_db_file.zip -d ./file/
 
 rm -r cardiffnlp
-python main.py --lda 'lda_6_9.pickle' \
-  --db 'url_check_6_8.db' \
+python main.py --lda 'lda_6_13.pickle' \
+  --db 'preproc_6_2.csv' \
   --n_topic 10 \
   --sentiment "cardiffnlp/twitter-roberta-base-sentiment-latest" \
   --topic_text_dir './doc_topic/' \
