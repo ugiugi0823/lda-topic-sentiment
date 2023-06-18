4821fb884ee1b78b54d9ceb2e75689f412a5d58e
Hyunwook Jo <acerghjk@gmail.com>
2023-06-19 02:31:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,12 +22,14 @@ if __name__ == '__main__':
   p.add_argument("--n_topic", type=int , default=10, help="몇개의 토픽인지 적어주세요!")
   p.add_argument("--topic_dir", default="./lda_topic_sentiment/result", help="토픽별 감성분석 결과를 담는 경로")
   p.add_argument("--topic_text_dir", default="./lda_topic_sentiment/topic_text", help="lda 후 나온 토픽별 csv 경로")
+  p.add_argument("--drive", action='store_true', help="Drive 저장하고 싶으면")
 
   args = p.parse_args()
   setup(args)
   get_topic_text(args)
   sentiment(args)
-  get_score()
+  #get_score()
+
   
 
 
