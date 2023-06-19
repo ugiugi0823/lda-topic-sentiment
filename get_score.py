bf40ad2655ffcea137fb641d0c2ea7343d819d45
Hyunwook Jo <acerghjk@gmail.com>
2023-06-19 13:34:10 +0900

    Update get_score.py

--- get_score.py
+++ get_score.py
@@ -6,13 +6,14 @@ snp = [4145.58, 4115.24, 4151.28, 4205.45, 4205.52, 4205.52, 4205.52, 4205.52, 4
        4273.79, 4283.85, 4267.52, 4293.93]
 
 
-def get_score():
+def get_score(args):
   for window_size in range(1, 6):
     print(f'window size: {window_size}')
     for i in range(10):
-        df = pd.read_csv(
-            './sentiment_result/' + 'result_topic_' + str(
-                i) + '_text.csv', lineterminator='\n')
+        if args.drive:
+          df = pd.read_csv('/content/drive/MyDrive/inisw08/lda-topic-sentiment/sentiment_result/' + 'result_topic_' + str(i) + '_text.csv', lineterminator='\n')
+        else:
+          df = pd.read_csv('/content/inisw08/lda-topic-sentiment/sentiment_result/' + 'result_topic_' + str(i) + '_text.csv', lineterminator='\n')
         print(f'토픽: {i}_______________________________________')
         #수정된 결과: 윈도우 사이즈 별로 집계된 df와 상관계수가 토픽별로 나옴
         print(get_correlation(df, snp, i+1))
